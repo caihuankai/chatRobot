9084b3b087eadc9babd49ff34983601603e2513e
caihuankai <847516867@qq.com>
2019-03-01 17:58:05 +0800

    add stock analysis

--- server/application/api/controller/Index.php
+++ server/application/api/controller/Index.php
@@ -7,10 +7,100 @@ use think\Request;
 class Index extends App
 {
     
-    public function index()
-    {
-        echo 'hello world';
-        return;
+    public function index(){
+
     }
-    
+    public function tulingApi(){
+    	$arr = array(
+			'reqType' => '0',//
+			'perception' => array(
+				'inputText' => array(
+					'text' => '时间'
+				)
+			),
+			'userInfo' => array(
+				'apiKey' => '44f49fc37dd8487382f5875d460f138d',
+				'userId' => '123'
+			)
+		);
+		$arr = json_encode($arr);
+		$msg = $this->call_tuling_api($arr);
+		return json_decode($msg,true)['results'][0]['values']['text'];
+    }
+
+    function call_tuling_api($array,$posturl="http://openapi.tuling123.com/openapi/api/v2"){
+    	$ch = curl_init();
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $array);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_URL, $posturl);
+        $return_data = curl_exec($ch);
+        curl_close($ch);
+        return $return_data;
+	}
+
+	public function stockAnalysis($stock_code='000001'){
+		$request_url = sprintf("http://doctor.10jqka.com.cn/%s/",$stock_code);
+		$data = file_get_contents($request_url);
+		$data = iconv("gb2312", "utf-8//IGNORE",$data);
+		//匹配title正则
+		$title_partern = '/<strong class="title">(.*?)<a href="#" class="more hide">/ies';
+		preg_match_all($title_partern,$data,$title_result);
+		$result['title'] = $title_result[1][0];
+		//匹配趋势则
+		$duanqi_qushi_partern = '/<span>短期趋势：<\/span><p>(.*?)<\/p>/ies';
+		preg_match($duanqi_qushi_partern,$data,$duanqi_qushi_result);
+		$result['duanqi_qushi'] = $duanqi_qushi_result[1];
+		$zhongqi_qushi_partern = '/<span>短期趋势：<\/span><p>(.*?)<\/p>/ies';
+		preg_match($zhongqi_qushi_partern,$data,$zhongqi_qushi_result);
+		$result['zhongqi_qushi'] = $zhongqi_qushi_result[1];
+		$zhongqi_qushi_partern = '/<span>中期趋势：<\/span><p>(.*?)<\/p>/ies';
+		preg_match($zhongqi_qushi_partern,$data,$zhongqi_qushi_result);
+		$result['zhongqi_qushi'] = $zhongqi_qushi_result[1];
+		$changqi_qushi_partern = '/<span>长期趋势：<\/span><p>(.*?)<\/p>/ies';
+		preg_match($changqi_qushi_partern,$data,$changqi_qushi_result);
+		$result['changqi_qushi'] = $changqi_qushi_result[1];
+		//近期成本
+		$jinqi_chengben_partern = '/<p class="content">(.*?)<\/p>/ies';
+		preg_match($jinqi_chengben_partern,$data,$jinqi_chengben_result);
+		$result['jinqi_chengben'] = $jinqi_chengben_result[1];
+		$result['jinqi_chengben'] = strip_tags($result['jinqi_chengben']);
+		//市场表现
+		$shichang_biaoxian_partern = '/<h3 class="hd">市场表现<\/h3>(.*?)<\/div>/ies';
+		preg_match($shichang_biaoxian_partern,$data,$shichang_biaoxian_result);
+		$result['shichang_biaoxian'] = $shichang_biaoxian_result[1];
+		$result['shichang_biaoxian'] = strip_tags($result['shichang_biaoxian']);
+		//压力支撑
+		$yali_zhicheng_partern = '/压力支撑<\/h3>(.*?)<\/div>/ies';
+		preg_match($yali_zhicheng_partern,$data,$yali_zhicheng_result);
+		$result['yali_zhicheng'] = $yali_zhicheng_result[1];
+		$result['yali_zhicheng'] = strip_tags($result['yali_zhicheng']);
+		//多空趋势
+		$duokong_qushi_partern = '/<h3 class="hd">多空趋势<\/h3>(.*?)<\/div>/ies';
+		preg_match($duokong_qushi_partern,$data,$duokong_qushi_result);
+		$result['duokong_qushi'] = $duokong_qushi_result[1];
+		$result['duokong_qushi'] = strip_tags($result['duokong_qushi']);
+		//主力分析
+		//资金流向
+		$zijin_liuxiang_partern = '/如何查看实时大资金异动？<\/a>(.*?)<\/div>/ies';
+		preg_match($zijin_liuxiang_partern,$data,$zijin_liuxiang_result);
+		$result['zijin_liuxiang'] = $zijin_liuxiang_result[1];
+		$result['zijin_liuxiang'] = strip_tags($result['zijin_liuxiang']);
+		//主力控盘
+		$zhuli_kongpang_partern = '/主力控盘<\/a>(.*?)<\/div>/ies';
+		preg_match($zhuli_kongpang_partern,$data,$zhuli_kongpang_result);
+		$result['zhuli_kongpang'] = $zhuli_kongpang_result[1];
+		$result['zhuli_kongpang'] = strip_tags($result['zhuli_kongpang']);
+		//机构持仓
+		$jigou_chicang_partern = '/机构持仓<\/a>(.*?)<\/div>/ies';
+		preg_match($jigou_chicang_partern,$data,$jigou_chicang_result);
+		$result['jigou_chicang'] = $jigou_chicang_result[1];
+		$result['jigou_chicang'] = strip_tags($result['jigou_chicang']);
+		//数据拼接
+		$dataConnect = "【大数据分析】"."\n".$result['title']."\n"."【持仓建议】"."\n"."短线建议：".$result['duanqi_qushi']."\n"."中线建议：".$result['zhongqi_qushi']."\n"."长线建议：".$result['changqi_qushi']."\n"."【近期表现】"."\n".$result['jinqi_chengben']."\n".$result['shichang_biaoxian']."\n".$result['yali_zhicheng']."\n".$result['duokong_qushi']."\n".$result['zijin_liuxiang']."\n"."【主力分析】"."\n".$result['zhuli_kongpang']."\n".$result['jigou_chicang'];
+		// return $data; 
+		return $dataConnect;
+	}
 }
--- server/application/index/controller/Index.php
+++ server/application/index/controller/Index.php
@@ -6,7 +6,71 @@ use think\Request;
 use app\common\model\Users;
 
 class Index extends App
-{
+{   
+    /**
+    * array:
+    * @array $key (requirement) 注册之后在机器人接入页面获得(32位)
+    * @array $info (requirement)请求内容，编码方式为 UTFUTFUTF-8 (1-30位)
+    * @array $userid (requirement) 开发者给自己的用户分配的唯一标志 (1-32位)
+    * @array $loc (optional) 地址
+    *
+    * @param $posturl (requirement) 接口，默认为"http://www.tuling123.com/openapi/api";
+    *
+    * @return string
+    */
+    function call_tuling_api( $array,$posturl="http://www.tuling123.com/openapi/api" ){
+        if (empty($array) || !is_array($array)){
+            return 'ERROR: Sorry , your param are error. --- nosee';
+        }
+        $jsoninfo = json_encode($array);  //把传入的数据进行json编码
+        //模拟post请求
+        $ch = curl_init();//初始化curl
+        if ( $ch === FALSE ){
+            return 'ERROR: Sorry , you cannot open curl. --- nosee';
+        }
+        curl_setopt($ch, CURLOPT_URL,$posturl); //抓取指定网页
+        curl_setopt($ch, CURLOPT_HEADER, 0);    //设置header
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            "Content-type:application/json;charset=utf-8",
+            "Content-Length: " . strlen($jsoninfo)
+        ));     //设置head头的请求数据格式为json
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//要求结果为字符串且输出到屏幕上
+        curl_setopt($ch, CURLOPT_POST, 1);   //post提交方式
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $jsoninfo);
+        $data = curl_exec($ch);   //运行curl 返回请求的json数据
+        $json = json_decode($data);  //把json数据转为php的对象类型
+        curl_close($ch);   //关闭curl
+        $content = $json->text;  //获取返回的文本信息
+        //处理返回的数据---超链接
+        if (!empty($json->url)){
+            $content .= "<a href=' ".$json->url." '>打开页面 </a>";
+        }
+        //处理返回的数据---列表
+        if (!empty($json->list)){
+            $i=0;
+            //新闻类
+            if ($json->code == 302000) {
+                $newinfo='';
+                foreach ($json->list as $list_item) {
+                    $newinfo.="\n【".++$i."】<a href=' ".$list_item->detailurl." '>".$list_item->article."</a>";
+
+                    if ($i==5)   break;
+                }
+                $content .=$newinfo;
+            }
+            //菜谱类
+            if ($json->code == 308000) {
+                $menuinfo='';
+                foreach ($json->list as $list_item) {
+                    $menuinfo.="\n【".++$i."】 <a href=' ".$list_item->detailurl." '>".$list_item->name."</a>\n".$list_item->info;
+
+                    if ($i==5)   break;
+                }
+                $content .=' -- 共'.count($json->list)."项\n".$menuinfo;
+            }
+        }
+        return $content;
+    }
     public function _empty($name) 
     {
         echo 'cant found method: '.$name;
@@ -14,10 +78,19 @@ class Index extends App
     
     public function index()
     {
-        //return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_bd568ce7058a1091"></thinkad>';
-        //var_dump($)
-        echo 'hello world';
-        return;
+        $arr = array(
+
+            'key' => '44f49fc37dd8487382f5875d460f138d',//这里填写自己的机器人密钥
+
+            'info' => '明天',
+
+            'userid' => '9934455',
+
+            'loc' => ''
+
+        );
+
+        echo  call_tuling_api($arr);
     }
     
     public function test()
